3e35e01a0a09152551a6be27d0418b9afd0f6193
AshRepartoFerramenta <dcapod07@hotmail.com>
2020-05-20 14:58:54 +0200

    Js 1th Exercise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+var nome = prompt('Qual\'è il tuo nome');
+    console.log(nome);
+var cognome = prompt('Qual\'è il tuo cognome');
+    console.log(cognome);
+var colorePreferito= prompt('Qual\'è il tuo colore preferito');
+    console.log(colorePreferito);
+var anno= '19';
+    console.log(anno);
+var risultato = alert(nome + cognome + colorePreferito + anno);
+    console.log(risultato);
